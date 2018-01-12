ad70704d92097f84a0361b3391880344d05382b9
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-12 13:02:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+function add(a, b) {
+  return (a + b);
+}
+  expect(add(a, b)).toEqual(a + b)
+
+
+it('subtract(a, b) subtracts b from a and returns the result', function() {
+  expect(subtract(a, b)).toEqual(a - b)
+})
+
+it('multiply(a, b) multiplies two numbers and returns the result', function() {
+  expect(multiply(a, b)).toEqual(a * b)
+})
+
+it('divide(a, b) divides a by b and returns the result', function() {
+  expect(divide(a, b)).toEqual(a / b)
+})
+
+it('inc(n) increments n and returns the result', function() {
+  expect(inc(a)).toEqual(a + 1)
+})
+
+it('dec(n) decrements n and returns the result', function() {
+  expect(dec(a)).toEqual(a - 1)
+})
+
 function makeInt(n) {
   return (parseInt(n, 10));
 }
